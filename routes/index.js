6a2efebe3906a08d0def2c17435b54ef30e87c37
AaronHerera98 <aaron.ssss62@gmail.com>
2021-04-13 19:16:05 -0500

    templates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('recommendationPanel', { title: 'Express' });
 });
 
 router.get('/subir', (req, res) => {
